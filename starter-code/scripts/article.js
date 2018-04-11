4f6793cb6bc99887e5fd2ef2b2588772f64a2f0f
Alexander Stone <aloecend@gmail.com>
2018-04-11 11:09:29 -0700

    added function to convert Articles to html.

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -3,30 +3,30 @@
 let articles = [];
 
 // COMMENT: What is the purpose of the following function? Why is its name capitalized? Explain the context of "this" within the function. What does "rawDataObj" represent?
-// PUT YOUR RESPONSE HERE
+// It constructs an instance of the Article class. It's name is capitalized because class names should be capitalized. This refers to the current intance of the class. rawDataObj represents everything bibliographically including the article.
 
 function Article (rawDataObj) {
-  // TODO: Use the JS object that is passed in to complete this constructor function:
-  // Save ALL the properties of `rawDataObj` into `this`
+  this.title = rawDataObj.title;
+  this.category = rawDataObj.category;
+  this.author = rawDataObj.author;
+  this.authorUrl = rawDataObj.authorUrl;
+  this.publishedOn = rawDataObj.publishedOn;
+  this.body = rawDataObj.body;
 }
 
 Article.prototype.toHtml = function() {
   // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
-  // PUT YOUR RESPONSE HERE
+  // It deep copies everything so all sub-elements of the article template are also copied.
 
   let $newArticle = $('article.template').clone();
-  /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
 
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category);
-
-  /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
-    We need to fill in:
-      1. author name,
-      2. author url,
-      3. article title,
-      4. article body, and
-      5. publication date. */
+  $newArticle.find('header > div.byline > address > a').text(this.author);
+  $newArticle.find('header > div.byline > address > a').attr('href', this.authorUrl)
+  $newArticle.find('header > h1').text(this.title);
+  $newArticle.find('section.article-body').html(this.body);
+  $newArticle.find('header > div.byline > time').attr('datetime', this.publishedOn);
 
   // REVIEW: Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
